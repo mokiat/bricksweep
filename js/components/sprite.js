d0a29d6dfe4d36dfcbcc4b1b9c3f63579a57411e
Momchil Atanasov <momchil.dev@gmail.com>
2018-10-30 21:43:35 +0200

    feat(*): add sprite component

---
+++ js/components/sprite.js
@@ -0,0 +1,10 @@
+'use strict';
+
+class SpriteComponent {
+    constructor() {
+        this.width = 64;
+        this.height = 64;
+        this.opacity = 1.0;
+        this.spriteClass = 'typeblack';
+    }
+}
