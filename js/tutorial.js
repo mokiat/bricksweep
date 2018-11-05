fbf7434719edddd4ae5feb9cdc8606c0aa95cd16
Momchil Atanasov <momchil.dev@gmail.com>
2018-11-05 22:42:10 +0200

    feat(tutorial): stop video on close

--- js/tutorial.js
+++ js/tutorial.js
@@ -4,4 +4,14 @@ class TutorialFrame extends Frame {
     constructor(frameManager) {
         super(frameManager, '#tutorial');
     }
+
+    hide() {
+        // hackish way to stop the youtube video on tutorial close.
+        // the official API is cumbersome and the 'postMessage' approach
+        // offers no compatibility guarantees.
+        this.jqElement.find('#tutorial-player').each(function () {
+            this.src += '';
+        })
+        Frame.prototype.hide.call(this);
+    }
 }
